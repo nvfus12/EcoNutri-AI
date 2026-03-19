53c229b792dcd475e427654b88e7848254bb83a4
Auraz <111996762+AKANEMO22@users.noreply.github.com>
2026-03-19 17:26:14 +0700

    update

--- temp_schema.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('database/econutri.db')
-c = conn.cursor()
-c.execute("SELECT sql FROM sqlite_master WHERE type='table' and name in ('regional_specialties', 'nutrition_reference', 'specialty_nutrition', 'specialty_seasons', 'season');")
-for r in c.fetchall():
-    print(r[0])
-    print('---')
-conn.close()
